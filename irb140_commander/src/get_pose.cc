3ef46c4ad9280c103dc6d48b9069809e68c4f036
GitSRealpe <msn-realpe@hotmail.com>
2022-03-23 18:28:01 -0500

    cleaning and formating

--- irb140_commander/src/get_pose.cc
+++ irb140_commander/src/get_pose.cc
@@ -1,43 +1,45 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
-#include <moveit_visual_tools/moveit_visual_tools.h>
 
 #include "std_msgs/String.h"
-#include "irb140_commander/PoseRPY.h"
 #include <sstream>
 #include "iostream"
 
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+// #include <tf2/LinearMath/Quaternion.h>
+// #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+// #include <tf2_eigen/tf2_eigen.h>
+// #include <Eigen/Geometry>
+// #include <irb140_commander/RPY.h>
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "get_pose");
   ros::NodeHandle node_handle;
   ros::AsyncSpinner spinner(2);
   spinner.start();
+  ros::Rate loop_rate(2);
 
   moveit::planning_interface::MoveGroupInterface move_group("irb140_arm");
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-  const robot_state::JointModelGroup* joint_model_group =
-      move_group.getCurrentState()->getJointModelGroup("irb140_arm");
-
   ros::Publisher pub = node_handle.advertise<geometry_msgs::Pose>("robot_commander/pose_actual", 1000);
-  ROS_INFO("Pose actual del robot...");
-  ros::Rate loop_rate(10);
+
+  geometry_msgs::Pose pose;
+  // Eigen::Quaterniond q;
+  // Eigen::Vector3d ypr;
   while (ros::ok())
   {
-    geometry_msgs::Pose pose = move_group.getCurrentPose().pose;
-    std::cout<<pose<<"\n";
+    ROS_INFO("Pose actual del robot...");
+    pose = move_group.getCurrentPose().pose;
+    std::cout << pose << "\n";
+    // std::cout << pose.position;
+    // q.x() = pose.orientation.x;
+    // q.y() = pose.orientation.y;
+    // q.z() = pose.orientation.z;
+    // q.w() = pose.orientation.w;
+    // ypr = q.toRotationMatrix().eulerAngles(2, 1, 0);
+    // std::cout << ypr << "\n";
+
     pub.publish(pose);
-
-    ros::spinOnce();
-
+    // ros::spinOnce();
     loop_rate.sleep();
   }
   return 0;
