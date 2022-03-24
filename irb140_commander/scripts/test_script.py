9f712d7361d30e4c6b1ce52a10bd0c1b5de12f2e
GitSRealpe <msn-realpe@hotmail.com>
2022-03-24 17:24:00 -0500

    testing joint action

--- irb140_commander/scripts/test_script.py
+++ irb140_commander/scripts/test_script.py
@@ -1,39 +1,42 @@
-#!/usr/bin/env python
-from tf.transformations import quaternion_from_euler
-from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import PoseArray
-import std_msgs.msg
-from irb140_commander.msg import PoseRPY
-
-
+#! /usr/bin/env python
 import rospy
-import copy
+import sys
 
-pose_pub = rospy.Publisher('robot_commander/cmd_pose', PoseRPY, queue_size=10)
-rospy.init_node('pathSender', anonymous=True)
-rate = rospy.Rate(2) # 10hz
-rate.sleep()
+import actionlib
 
-punto=PoseRPY()
+# Brings in the messages used by the fibonacci action, including the
+# goal message and the result message.
+import irb140_commander.msg
 
-#suelta objeto
 
-punto.position.x=0.0;punto.position.y=-0.65;punto.position.z=0.6
-punto.rpy.roll=0;punto.rpy.pitch=0;punto.rpy.yaw=-1.57
-pose_pub.publish(punto)
-print("enviado")
-msg=rospy.wait_for_message('/robot_commander/pose_done',std_msgs.msg.String)
-print("trayectoria completada")
+def fibonacci_client():
+    # Creates the SimpleActionClient, passing the type of the action
+    # (FibonacciAction) to the constructor.
+    client = actionlib.SimpleActionClient("joints", irb140_commander.msg.JointsAction)
 
-punto.position.x=0.0;punto.position.y=-0.5;punto.position.z=0.4
-pose_pub.publish(punto)
-print("enviado")
-msg=rospy.wait_for_message('/robot_commander/pose_done',std_msgs.msg.String)
-print("trayectoria completada")
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
 
-punto.position.x=0.5;punto.position.y=0.1;punto.position.z=0.3
-punto.rpy.pitch=1.57;punto.rpy.yaw=0
-pose_pub.publish(punto)
-print("enviado")
-msg=rospy.wait_for_message('/robot_commander/pose_done',std_msgs.msg.String)
-print("trayectoria completada")
+    # Creates a goal to send to the action server.
+    goal = irb140_commander.msg.JointsGoal(joints=[0, 0, 0, 0, 0, 0])
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()  # A FibonacciResult
+
+
+if __name__ == "__main__":
+    try:
+        # Initializes a rospy node so that the SimpleActionClient can
+        # publish and subscribe over ROS.
+        rospy.init_node("fibonacci_client_py")
+        result = fibonacci_client()
+        print("Result:", result.estado)
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion", file=sys.stderr)
